b1769246ba99e92a2fb1740e95b5f0872bafe860
aliraees655123 <96068596+aliraees655123@users.noreply.github.com>
2023-07-23 13:13:02 +0500

    s

---
+++ src/constants/index.js
@@ -0,0 +1,434 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+
+} from '../assets';
+import primeSoft from './../assets/projects/primeSoft.png'
+import yakala from './../assets/projects/yakala.png'
+import kasowitz from './../assets/projects/kasowitz.png'
+import roadmap from './../assets/projects/roadmap.png'
+import norand from './../assets/projects/norand.png'
+import xenode from './../assets/projects/xenode.png'
+import tdc from './../assets/projects/tdc.png'
+import tch from './../assets/projects/tch.png'
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Frontend Developer',
+    icon: frontend,
+  },
+  {
+    title: 'Backend Developer',
+    icon: backend,
+  },
+  // {
+  //   title: 'UI/UX Design',
+  //   icon: ux,
+  // },
+  // {
+  //   title: 'Software Prototyping',
+  //   icon: prototyping,
+  // },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  // {
+  //   name: 'TypeScript',
+  //   icon: typescript,
+  // },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Redux Toolkit',
+    icon: redux,
+  },
+
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+
+
+ 
+  {
+    name: 'git',
+    icon: git,
+  },
+ 
+ 
+];
+
+const experiences = [
+  {
+    title: 'Front-End Developer',
+    company_name: 'Cover Hunt',
+    icon: coverhunt,
+    iconBg: '#333333',
+    date: 'Aug 2021 - Feb 2022',
+  },
+  {
+    title: 'Mentor (Volunteer)',
+    company_name: 'Microverse',
+    icon: microverse,
+    iconBg: '#333333',
+    date: 'Mar 2022 - May 2022',
+  },
+  {
+    title: 'Junior Software Engineer',
+    company_name: 'Kelhel',
+    icon: kelhel,
+    iconBg: '#333333',
+    date: 'May 2022 - Oct 2022',
+  },
+  {
+    title: 'Full Stack Developer',
+    company_name: 'Diversity Cyber Council',
+    icon: dcc,
+    iconBg: '#333333',
+    date: 'Sep 2022 - Present',
+  },
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'Bandi',
+    
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: komikult,
+    // repo: 'https://github.com/shaqdeff/KomiKult',
+    demo: 'https://romantic-pasteur-4db081.netlify.app/',
+  },
+  {
+    id: 'project-2',
+    name: 'Public Compare',
+   
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: leaderboard,
+   
+    demo: 'https://shimmering-lily-08196f.netlify.app/',
+  },
+  {
+    id: 'project-3',
+    name: 'Infinity Void',
+
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: math,
+ 
+    demo: 'https://magical-conkies-6d33d4.netlify.app/',
+  },
+  {
+    id: 'project-4',
+    name: 'Media',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: movie,
+
+    demo: 'https://moonlit-douhua-518648.netlify.app/',
+  },
+  {
+    id: 'project-5',
+    name: 'Drops Group',
+   
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: nyeusi,
+
+    demo: 'https://sunny-fenglisu-b2f602.netlify.app/',
+  },
+  {
+    id: 'project-6',
+    name: 'Prime Soft',
+   
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: primeSoft,
+
+    demo: 'https://fluffy-youtiao-31edcb.netlify.app/',
+  },
+  {
+    id: 'project-7',
+    name: 'Yakala',
+   
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: yakala,
+
+    demo: 'https://resilient-eclair-f12122.netlify.app/',
+  },
+  {
+    id: 'project-8',
+    name: 'Kasowtiz',
+   
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: kasowitz,
+
+    demo: 'https://subtle-starlight-21be85.netlify.app/',
+  },
+  {
+    id: 'project-9',
+    name: 'Roadmap',
+   
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: roadmap,
+
+    demo: 'https://melodic-fox-650ccb.netlify.app/',
+  },
+  {
+    id: 'project-10',
+    name: 'Norand Marphous',
+   
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: norand,
+
+    demo: 'https://norand-morphos.netlify.app/',
+  },
+  {
+    id: 'project-10',
+    name: 'Xenode',
+   
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: xenode,
+
+    demo: 'https://wondrous-entremet-155d10.netlify.app/User',
+  },
+  {
+    id: 'project-10',
+    name: 'TDC',
+   
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: tdc,
+
+    demo: 'https://subtle-lollipop-626bf6.netlify.app/',
+  },
+  {
+    id: 'project-10',
+    name: 'TheCryptoHub',
+   
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: tch,
+
+    demo: 'https://thecryptohub.com/',
+  },
+];
+
+export { services, technologies, experiences, projects };
